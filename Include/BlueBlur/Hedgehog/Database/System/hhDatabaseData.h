071b88c84e60f9f095dcafe1a39559055c55fc99
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-03-10 13:51:52 +0300

    Add Validate function to CDatabaseData in BlueBlur

--- Include/BlueBlur/Hedgehog/Database/System/hhDatabaseData.h
+++ Include/BlueBlur/Hedgehog/Database/System/hhDatabaseData.h
@@ -7,11 +7,20 @@
 
 namespace Hedgehog::Database
 {
+    class CDatabaseData;
+
+    static inline FUNCTION_PTR(bool, __thiscall, fpCDatabaseDataValidate, 0x6993E0, CDatabaseData* This);
+
     class CDatabaseData : public Base::CObject
     {
     public:
         uint8_t m_Flags;
         Base::CSharedString m_TypeAndName;
+
+        void Validate()
+        {
+            fpCDatabaseDataValidate(this);
+        }
     };
 
     ASSERT_OFFSETOF(CDatabaseData, m_Flags, 0x4);
