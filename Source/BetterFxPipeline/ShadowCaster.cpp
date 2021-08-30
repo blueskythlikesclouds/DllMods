70bcf2c74f363b14fb392ecb5f51b47e8663829f
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-08-30 21:17:26 +0300

    Revert "Implement force shadow casting for objects in MTFx"
    This reverts commit 4e960ec1fd69f06d6cdb7a89d0acfe08dc8bed32.

--- Source/BetterFxPipeline/ShadowCaster.cpp
+++ Source/BetterFxPipeline/ShadowCaster.cpp
@@ -24,36 +24,7 @@ void ShadowCaster::applyPatches()
 
     if (Configuration::forceCastShadow)
     {
-        // FxPipeline
         WRITE_MEMORY(0xEBEF87, uint8_t, 0x30, 0xC6, 0x00, 0x01);
         WRITE_NOP(0xEBEF8B, 20);
-
-        // MTFx
-        {
-            hh::fx::SDrawInstanceParam* childParams = (hh::fx::SDrawInstanceParam*)0x13DC908;
-            hh::fx::SDrawInstanceParam* newChildParams = new hh::fx::SDrawInstanceParam[4];
-
-            // Object
-            newChildParams[0] = childParams[0];
-            newChildParams[0].m_Unk0 = 1;
-
-            // Object Overlay
-            newChildParams[1] = childParams[0];
-            newChildParams[1].m_Unk0 = 6;
-
-            // Player
-            newChildParams[2] = childParams[0];
-            newChildParams[2].m_Unk0 = 8;
-
-            // Shadow Map Terrain
-            newChildParams[3] = childParams[1];
-
-            // Pass new children
-            WRITE_MEMORY(0x13DD27C, void*, newChildParams);
-            WRITE_MEMORY(0x13DD280, uint32_t, 4);
-
-            WRITE_MEMORY(0x13DD2FC, void*, newChildParams);
-            WRITE_MEMORY(0x13DD300, uint32_t, 4);
-        }
     }
 }
