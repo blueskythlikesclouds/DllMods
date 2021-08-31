cecf022779dbe78ddad7bf84127f045e36614f78
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-08-31 23:05:57 +0300

    Simplify post processing on particles code

--- Source/BetterFxPipeline/ParticlePostProcessor.cpp
+++ Source/BetterFxPipeline/ParticlePostProcessor.cpp
@@ -26,34 +26,18 @@ void ParticlePostProcessor::applyPatches()
     // MTFx
     //
     {
-        // Expand GameScene's Render Scene Base to include Render Particle
+        // Append Render Particle's last child to Render Scene Base
         hh::fx::SDrawInstanceParam* renderSceneBase = (hh::fx::SDrawInstanceParam*)0x13DDC88;
-        hh::fx::SDrawInstanceParam* renderParticle = (hh::fx::SDrawInstanceParam*)0x13DD778;
 
         hh::fx::SDrawInstanceParam* newChildren = new hh::fx::SDrawInstanceParam[renderSceneBase->m_ChildParamCount + 1];
         memcpy(newChildren, renderSceneBase->m_ChildParams, renderSceneBase->m_ChildParamCount * sizeof(hh::fx::SDrawInstanceParam));
-
-        hh::fx::SDrawInstanceParam* newChild = &newChildren[renderSceneBase->m_ChildParamCount];
-
-        *newChild = *renderParticle;
-        newChild->m_DepthStencilSurface = renderSceneBase->m_DepthStencilSurface;
-        newChild->m_RenderTargetSurface = renderSceneBase->m_RenderTargetSurface;
-        newChild->m_MsaaDepthStencilSurface = renderSceneBase->m_MsaaDepthStencilSurface;
-        newChild->m_MsaaRenderTargetSurface = renderSceneBase->m_MsaaRenderTargetSurface;
-        newChild->m_ReflectionMap2Sampler = renderSceneBase->m_ReflectionMap2Sampler;
-        newChild->m_ReflectionMapSampler = renderSceneBase->m_ReflectionMapSampler;
-        newChild->m_VerticalShadowMapSampler = renderSceneBase->m_VerticalShadowMapSampler;
-        newChild->m_ShadowMapSampler = renderSceneBase->m_ShadowMapSampler;
-
-        // Render only normal particles in Render Scene Base
-        newChild->m_ChildParams = (char*)newChild->m_ChildParams + 2 * sizeof(hh::fx::SDrawInstanceParam);
-        newChild->m_ChildParamCount = 1;
+        memcpy(&newChildren[renderSceneBase->m_ChildParamCount], (void*)0x13DC8C8, sizeof(hh::fx::SDrawInstanceParam));
 
         // Pass new children
         WRITE_MEMORY(&renderSceneBase->m_ChildParams, void*, newChildren);
         WRITE_MEMORY(&renderSceneBase->m_ChildParamCount, uint32_t, renderSceneBase->m_ChildParamCount + 1);
 
         // Render only framebuffer/stencil particles in Render Particle
-        WRITE_MEMORY(&renderParticle->m_ChildParamCount, uint32_t, 2);
+        WRITE_MEMORY(0x13DD790, uint32_t, 2);
     }
 }
