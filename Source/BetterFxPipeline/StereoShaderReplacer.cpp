7062f78d2473ae0d8afe8e943d6d6b90aa8f1430
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-10-24 22:51:14 +0300

    Fix 3D mode patch addresses

--- Source/BetterFxPipeline/StereoShaderReplacer.cpp
+++ Source/BetterFxPipeline/StereoShaderReplacer.cpp
@@ -9,8 +9,8 @@ void StereoShaderReplacer::applyPatches()
 
     enabled = true;
 
-    WRITE_MEMORY(0x11C965F, char, 'r');
-    WRITE_MEMORY(0x11C966B, char, 'r');
-    WRITE_MEMORY(0x11C96CB, char, 'r');
-    WRITE_MEMORY(0x11C96DB, char, 'r');
+    WRITE_MEMORY(0x15CA65F, char, 'r');
+    WRITE_MEMORY(0x15CA66B, char, 'r');
+    WRITE_MEMORY(0x15CA6CB, char, 'r');
+    WRITE_MEMORY(0x15CA6DB, char, 'r');
 }
