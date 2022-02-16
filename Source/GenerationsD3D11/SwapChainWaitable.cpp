5f289fe5454cd47b0643035c5c1634490f95ad2d
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2022-02-17 01:27:13 +0300

    [GenerationsD3D11] Fix maximum frame latency mistake.

--- Source/GenerationsD3D11/SwapChainWaitable.cpp
+++ Source/GenerationsD3D11/SwapChainWaitable.cpp
@@ -50,7 +50,7 @@ bool SwapChainWaitable::initialize(Device* device, D3DPRESENT_PARAMETERS* presen
 
     swapChain1.As(&swapChain);
 
-    swapChain->SetMaximumFrameLatency(max(1, presentationParameters->BackBufferCount));
+    swapChain->SetMaximumFrameLatency(max(1, presentationParameters->BackBufferCount - 1));
     waitHandle = swapChain->GetFrameLatencyWaitableObject();
 
     ComPtr<ID3D11Texture2D> backBuffer;
